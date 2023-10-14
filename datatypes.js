07ad757b67ed89b1215d5813874958afc6519d86
Satyam Pratap <satyampratap007@gmail.com>
2023-10-14 15:06:25 +0530

    basic js

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+
+// Primitive data types - buit in datatypes in js(fundamental datatypes)
+// 7 in total
+/*
+N - null
+N - Number
+S - Symbol
+S - String
+B - Boolean
+B - BigInt
+U - undefined
+*/
+/*
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567")
+let e = "haary"
+let f = Symbol("I am a nice symbol")
+let g
+*/
+// console.log(a,b,c,d,e,f,g)
+
+// Non- Primitive data types - Objects(Key-value pair)
+const item = {
+    "satyam" : true,
+    "ram" : 45,
+    "shyam" : 67,
+    "geeta" : 75
+}
+
+console.log(item["satyam"])
---
+++ strings.js
@@ -0,0 +1,17 @@
+// console.log("Hello World")
+
+// Strings
+
+// console.log(`Ram is 
+// a
+// good 
+// boy`)  // multi line string
+console.log("Ram is\" good")
+// console.log('Ram + \'Shyam') // back slash escape
+// console.log("Ram + \n Shyam") // new line escape
+// console.log(`$${10+20}`) // Interpolation
+// // Here we can insert values directly in the strick using interpolation
+// // and we use it under back tick
+// var c = "Ram is a good boy"
+
+
