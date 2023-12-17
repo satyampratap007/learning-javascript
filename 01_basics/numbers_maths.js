017267ef64ac0fc0c714ae6933c885dd726dbaeb
Satyam Pratap <satyampratap007@gmail.com>
2023-12-17 18:41:27 +0000

    Learnt about numbers and inbuilt math function in js

--- 01_basics/numbers_maths.js
+++ 01_basics/numbers_maths.js
@@ -24,10 +24,16 @@
 
 // Random value will range from 0 to 1
 
-console.log(Math.random());
-console.log(Math.floor(Math.random()*10)+1); // 1 will make sure atleast 1 will come at minimum 
-const min = 10;
-const max = 20;
-console.log(Math.floor(Math.random() * (max - min + 1) + min));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1); // 1 will make sure atleast 1 will come at minimum 
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
 
+// Example
 
+// console.log(Math.random()*10+1);
+const min = 5;
+const max = 10;
+
+console.log(Math.floor(Math.random() *(max-min+1)) + min);
