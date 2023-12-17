d4dbb17555e7fc147d105e99e871b64bbb39a5ed
Satyam Pratap <satyampratap007@gmail.com>
2023-12-17 18:27:56 +0000

    updated the folder

---
+++ 01_basics/P_S_1.js
@@ -0,0 +1,35 @@
+/*
+let s = "45";
+let j = "65";
+console.log(s+j);  // concatenation
+console.log(typeof s+j) // String65
+console.log(typeof (s+j))
+*/
+
+/*
+const a = {
+    name : "Satyam",
+    section : 1,
+    isPrincipal: false
+}
+
+a = 54 // no we can't assign value to const
+a['friend'] = "Shubham" // but we can change under the object
+a['name'] = "lovish"
+console.log(a)
+
+*/
+const dictionary = {
+    game : "khel",
+    dance : "nachna",
+    run : "bhagna",
+    pee : "mootna",
+    fart : "padna"
+}
+console.log(dictionary)
+console.log(dictionary.dance)
+// or
+console.log(dictionary['fart'])
+
+
+
---
+++ 01_basics/numbers_maths.js
@@ -0,0 +1,33 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100.05555);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const num = 123.29566
+// console.log(num.toPrecision(4));
+
+// const temp = 10000000
+// console.log(temp.toLocaleString('en-IN'));
+
+
+
+// *********************** MATHS ****************************//
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// const l1 = Math.sqrt(8);
+// console.log(l1.toFixed(2));
+
+// Random value will range from 0 to 1
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1); // 1 will make sure atleast 1 will come at minimum 
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
