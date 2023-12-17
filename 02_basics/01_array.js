90c381077d559be37bc762e260d0680e9aecd825
Satyam Pratap <satyampratap007@gmail.com>
2023-12-17 19:54:32 +0000

    learnt about array ans its methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+// Array
+// size is resizable
+// can store different datatypes value in a single array
+// 0-based indexing 
+// Arrays create a shallow copy(pass the final reference of the array)
+
+const myArr = [1,2,3,4,5,6]
+// console.log(myArr[0]);
+// console.log(myArr.length);
+
+// const arr = new Array(1,2,3,4,5,6);
+// console.log(myArr[2]);
+
+// Array methods
+
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9);  // add the element in the first position
+// console.log(myArr);
+
+// myArr.shift();  // shift to one position ahead in the array that simply means to remove the 1st element
+// console.log(myArr);
+
+// console.log(myArr.includes(10)); // return boolean value
+// console.log(myArr.indexOf(15)); // if present then index else -1
+
+// const newArray = myArr.join();  // will bind and convert it into the string
+
+// console.log(myArr);
+// console.log(newArray);
+// console.log(typeof(myArr));  // object
+// console.log(typeof(newArray));  // string
+
+
+
+
