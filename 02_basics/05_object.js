63d2795ea0e4c7a13f501984a20271d6ce92d4d4
Satyam Pratap <satyampratap007@gmail.com>
2024-01-02 08:07:04 +0000

    object and array destructuring

---
+++ 02_basics/05_object.js
@@ -0,0 +1,27 @@
+// Object Destructuring & Array  Destructuring
+let course = {
+    C_name : "Jav Script",
+    price: "9999",
+    courseInst: "Satyam"
+}
+
+const {courseInst: inst} =  course;
+
+console.log(inst);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
