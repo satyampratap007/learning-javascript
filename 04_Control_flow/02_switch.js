9983ff247ed45ca74c7fd0440b92ef55c514525e
Satyam Pratap <satyampratap007@gmail.com>
2024-01-06 17:20:02 +0000

    Switch case

---
+++ 04_Control_flow/02_switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 1;
+switch (month){
+    case 1:
+         console.log("january");
+         break;
+    case 2:
+         console.log("February");
+         break;
+    case 3:
+         console.log("March");
+         break;
+    default:
+        console.log("None of the above");
+        
+}
