8c3acba01710d2ea728f19119b92179baabc407d
Satyam Pratap <satyampratap007@gmail.com>
2024-01-07 09:52:19 +0000

    do while

---
+++ 05_iterations/02_do_while.js
@@ -0,0 +1,17 @@
+// while
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// // do while
+// const i = 5;
+// do {
+//     console.log(i++);
+// } while (i < 5) {
+//     console.log(i);
+// }
+
+
