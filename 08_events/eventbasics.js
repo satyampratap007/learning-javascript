0a79852244a3fa65c96a0a8ad15eb54f7289fca0
Satyam Pratap <satyampratap007@gmail.com>
2024-01-31 14:37:48 +0530

    learnt about event propogation

---
+++ 08_events/eventbasics.js
@@ -0,0 +1,38 @@
+// document.getElementById("owl").onclick = () => {
+//     alert('this is a functional owl');
+// }
+
+
+// document.querySelector("#owl").addEventListener('click', () => {
+//     alert('owl clicked using event listener');
+// }, false); // here false is optional as a third parameter
+
+
+// using attach Event() => past 
+// jquery d- on
+
+
+// Event Object
+
+document.querySelector("#owl").addEventListener('click', (e) => {
+    console.log(e);
+}, false);
+
+// Type
+// Timestamp
+// defaultPrevented
+// target
+// toElement
+// srcElement
+// currentTarget
+// ClientX, ClientY, screenX, ScreenY 
+// altkey, keyCode, shiftkey, ctrlkey
+
+// Event propagation -> Two Context
+// 1) Event Bubbling
+// 2) Event Capturing
+
+
+
+
+
