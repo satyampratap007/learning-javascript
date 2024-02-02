5231f6c9cbd0b7572fdfda5135bd5bf79a087f0f
Satyam Pratap <satyampratap007@gmail.com>
2024-02-03 01:08:17 +0530

    done baic tasks on event

--- 08_events/eventbasics.js
+++ 08_events/eventbasics.js
@@ -14,9 +14,9 @@
 
 // Event Object
 
-document.querySelector("#owl").addEventListener('click', (e) => {
-    console.log(e);
-}, false);
+// document.querySelector("#owl").addEventListener('click', (e) => {
+//     console.log(e);
+// }, false);
 
 // Type
 // Timestamp
@@ -32,6 +32,38 @@ document.querySelector("#owl").addEventListener('click', (e) => {
 // 1) Event Bubbling
 // 2) Event Capturing
 
+// document.querySelector('#images').addEventListener('click', (e) => {
+//     console.log("Clicked inside the ul")
+// }, false)
+
+// document.querySelector('#owl').addEventListener('click', (e) => {
+//     console.log("Owl Clicked");
+// }, false)
+
+// to stop the default behaviour of anchor tag
+
+document.querySelector('#google').addEventListener('click', (e) => {
+    console.log("Google Clicked");
+    e.preventDefault();
+    // e.stopPropagation(); // to stop the further propogation
+},false)
+
+
+/***************** Exercise ******************/
+
+// When we click on any images then it should fade out
+
+document.querySelector('#images').addEventListener('click', (e) => {
+    // console.log(e.target.parentNode);
+    // console.log(remove_child);
+    // remove_child.remove();
+    // OR
+    const t = e.target; // target element
+    if(t.tagName === 'IMG'){
+        let rem = t.parentNode;
+        rem.parentNode.removeChild(rem);
+    }
+}, false)
 
 
 
